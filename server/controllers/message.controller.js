5e642cac42911d56153319eed4dc7fe5792f61b0
Thomas Ho <khoaho120703@gmail.com>
2026-02-24 22:47:50 +0700

    [server] feat: sendMessage API

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 import { ApiStatus } from "../constants/apiStatus.js";
-
+import { v2 as cloudinary } from "cloudinary";
 export const getUsers = async (req, res) => {
   try {
     const currentUserId = req.user._id;
@@ -85,6 +85,45 @@ export const getMessages = async (req, res) => {
     });
   } catch (error) {
     console.log("getMessages error:", error);
+    return res.status(ApiStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Internal server error.",
+    });
+  }
+};
+
+const uploadImage = async (image, messageId) => {
+  const uploadOptions = {
+    folder: "PulseChat/messages",
+    public_id: `message_${messageId}`,
+  };
+  return cloudinary.uploader.upload(image, uploadOptions);
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { receiverId, text, image } = req.body;
+    const senderId = req.user._id;
+
+    if (image) {
+      const result = await uploadImage(image, message._id);
+      message.image = result.secure_url;
+    }
+
+    const message = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: image ? result.secure_url : "",
+    });
+
+    return res.status(ApiStatus.CREATED).json({
+      success: true,
+      message: "Message sent successfully.",
+      results: message,
+    });
+  } catch (error) {
+    console.log("sendMessage error:", error);
     return res.status(ApiStatus.INTERNAL_SERVER_ERROR).json({
       success: false,
       message: "Internal server error.",
