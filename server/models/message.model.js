b0296858556dde8916c6f8e8770ebd621321c393
Thomas Ho <khoaho120703@gmail.com>
2026-02-24 21:29:08 +0700

    [server] feat: Message MOdel

---
+++ server/models/message.model.js
@@ -0,0 +1,33 @@
+import mongoose, { mongo } from "mongoose";
+
+const messageSchema = new mongo.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      trim: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
